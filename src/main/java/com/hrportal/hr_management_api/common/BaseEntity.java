dc6d26222062be39f9c66bea7e4ba007ce43d768
Vedant Patel <vedantdp2010@gmail.com>
2026-05-28 22:01:42 +0530

    chore: add base entity and API response wrapper

---
+++ src/main/java/com/hrportal/hr_management_api/common/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.hrportal.hr_management_api.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
